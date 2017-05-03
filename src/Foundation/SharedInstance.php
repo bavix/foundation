cb2c5d6d9ee873b9409ac06bce1eda51725b0439
REZ1DENT3 <maksim.babichev95@gmail.com>
2017-05-03 10:05:58 +0300

    patch

--- src/Foundation/SharedInstance.php
+++ src/Foundation/SharedInstance.php
@@ -11,16 +11,38 @@ trait SharedInstance
     protected static $instance;
 
     /**
+     * @param array ...$arguments
+     *
      * @return static
      */
-    public static function sharedInstance()
+    public static function sharedInstance(...$arguments)
     {
         if (!static::$instance)
         {
-            static::$instance = new static();
+            static::$instance = new static(...$arguments);
         }
 
         return static::$instance;
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @return void
+     */
+    final public function __wakeup()
+    {
+
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @return void
+     */
+    final private function __clone()
+    {
+
     }
 
 }
